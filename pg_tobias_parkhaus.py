86fdac9697aed018be5cdbbd7cf834367b82efe5
TobiasKerschensteiner <tobias-kerschensteiner@gmx.de>
2022-12-16 21:25:07 +0100

    variablen erstellt

--- pg_tobias_parkhaus.py
+++ pg_tobias_parkhaus.py
@@ -1,1 +1,14 @@
-#Hallo
+#wie viele Parkplätze sind da
+
+#zufallauswahl Parkplatz
+
+#welche Parkplätze sind da
+import random
+
+APark = 9
+
+zP = APark**0,5
+rP = APark**0,5
+
+print(zP)
+print(rP)
