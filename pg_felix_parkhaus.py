e0150887b14cbd44d8b957d02bbb354c5aaf5c1a
Felix-Doering <felix.doering111@gmail.com>
2022-12-21 16:46:22 +0100

    Aktuelle Version

--- pg_felix_parkhaus.py
+++ pg_felix_parkhaus.py
@@ -1,1 +1,16 @@
-test1
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Eingabefelder (Preis, Parkplatzanzahl, Geschwindigkeit)
+#Preis
+def inputprice(): #Funktion zur Eingabe des Preises
+    price = 0
+    price = float(input("Preis pro Stunde: "))
+    print("Preis/Std", price)
+
+inputprice()
+
+#Anzahl der Parkplätze
+def quantitylots(): #Funktion zur Anzahl der Parkplätze
