c362898757398145f499c54c562e373796e837f6
TobiasKerschensteiner <tobias-kerschensteiner@gmx.de>
2023-01-21 12:43:49 +0100

    Nach entfernen eines Autos wird die ptime des Nächsten nicht heruntergezählt - fixed
    Mehr in Funktionen verpackt
    
    Wichtige Listen "cars" und "lpark" sind jetzt globale Listen

--- pg_tobias_parkhaus.py
+++ pg_tobias_parkhaus.py
@@ -1,21 +1,23 @@
 import random
 import time
 carnr = 0
-q = 0
-pcount = 10                                     #Parkplatzanzahl
+
+pcount = 10                                    #Parkplatzanzahl
 timeh = 0                                       #aktuelle h
 timem = 0                                       #aktuelle min
 timed = 0                                       #aktueller Tag
 inp_velocity = 1000                             #Zeitbeschleunigungsfaktor
 maxpark = 30                                    #maximale Parkzeit
 minpark = 1                                     #minimale Parkzeit
-lpark = []                                      #leere Parkplätze
-cars = []                                       #Autos
+global lpark
+lpark = []                                      #leere Parkplaetze (global)
+global cars
+cars = []                                       #aktuelle Autos    (global)
 pplace = 0
 dhm = [0,0,0]
 hparked = 0
 
-def createcar (dhm,lpark,cars,carnr):
+def createcar (dhm,lpark,carnr):
 
     timed = dhm [0]
     timeh = dhm [1]
@@ -30,11 +32,11 @@ def createcar (dhm,lpark,cars,carnr):
     carrand = ["Car_green","Car_red","Car_pink","Car_blue","Car_gray"]
     carco = random.choice(carrand)
 
-    car = [carnr, pplace, timem, timem + ptime,ptime,carco]  # [Autonr. , parplatzNr. , Parkzeit Start, Parkzeit Ende, Dauer Parken]
+    car = [carnr, pplace, timem, timem + ptime,ptime,carco]  # [Autonr. , parplatzNr. , Parkzeit Start, Parkzeit Ende, Dauer Parken Rest, Autofarbe]
     cars.append(car)
 
 
-    return cars,lpark
+    return lpark
 
 def clock (timefactor,dhm):
 
@@ -56,6 +58,36 @@ def clock (timefactor,dhm):
     dhm = [timed,timeh,timem]
     return(dhm)
 
+def ptimecd_remvcar():
+    q = 0                               # Variable zum durcharbeiten von cars
+    codw = 0                            # Hilfsvariable zum herunterzaehlen der restlichen Parkzeit
+    hparked = 0                         # Stunden geparket in Stunden aufgerundet
+    if cars !=[]:
+        for q in range (len(cars)):  # Parkdauer wird runtergezählt (für jedes Auto in liste cars)
+            if cars[q] != [0]:
+                codw = cars[q][4]
+                codw = codw - 1
+                cars[q][4] = codw
+                if codw == 0:                           # Autos (Listen) in cars werden auf abgelaufene Parkzeiten überprüft
+                                                        # und wenn 0 dann aus cars entfernt und Parkplatznr. wird wieder in leere Parkplätze zurückgegeben.
+                    hparked = cars[q][3] - cars[q][2]
+                    hparked = hparked/60
+                    hparked = int(hparked)+1
+                    lpark.append(cars[q][1])            #gibt Parkplatznr als leer an Liste lpark zurück
+                    cars[q] = [0]
+                    print(f"hparked {hparked}")
+
+                                                        #Umsatz wird hier berechnet
+                codw = 0
+        o = 0                                       #Variable zum drucharbeiten von cars
+        print(cars)
+        while o < len(cars):
+            if cars[o] == [0]:
+                cars.remove(cars[o])
+            o = o+1
+        o = 0
+
+
 
 
 
@@ -67,42 +99,25 @@ print(lpark)
 for c in range (2000):
     dhm = clock(inp_velocity,dhm)
 
-
-
     if len(lpark) != 0:
 
         probcar = random.randint(0, 100)                              # Ob Auto erstellt wird, wird ausgewürfelt
         carnr = carnr+1                                                 # AutoNr. wird hochgezählt
-        if carnr >= 500:
-            carnr = 0
+        #if carnr >= 9999:                                            maybe max Autonumr.
+        #    carnr = 0
 
         if 0 == probcar%2 and 6 < timeh < 10:                         # Zeit und Wahrscheinlichkeit für erstellen eines Autos wird bestimmt (Vormittag)
-           cars, lpark = createcar(dhm,lpark,cars,carnr)
+           lpark = createcar(dhm,lpark,carnr)
 
         if 0 == probcar%2 and timeh < 6 or timeh > 10:                # Zeit und Wahrscheinlichkeit für erstellen eines Autos wird bestimmt (Rest der Zeit)
-            cars, lpark = createcar(dhm,lpark,cars,carnr)
+            lpark = createcar(dhm,lpark,carnr)
 
 
 
 
 
+    ptimecd_remvcar()
 
-    while q < len(cars) and cars !=[]:  # Parkdauer wird runtergezählt (für jedes Auto in liste cars)
-        codw = cars[q][4]
-        codw = codw - 1
-        cars[q][4] = codw
-        if codw == 0:                           # Autos (Listen) in cars werden auf abgelaufene Parkzeiten überprüft
-                                                # und wenn 0 dann aus cars entfernt und Parkplatznr. wird wieder in leere Parkplätze zurückgegeben.
-            hparked = cars[q][3] - cars[q][2]
-            hparked = hparked/60
-            hparked = int(hparked)+1
-            lpark.append(cars[q][1])
-            cars.remove(cars[q])
-            print(f"hparked {hparked}")
-                                                #Umsatz wird hier berechnet
-        q = q+1
-        codw = 0
-    q = 0
 
 
     print(cars)
